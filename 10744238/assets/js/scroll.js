8db788924200c5759ae7bf83b7e5a302ec81be6e
yunnn11 <61927834+yunnn11@users.noreply.github.com>
2020-05-13 00:07:08 +0800

    Add files via upload

---
+++ 10744238/assets/js/scroll.js
@@ -0,0 +1,23 @@
+<script>
+
+       $(function () {
+
+           $(‘a[href*=#]:not([href=#])’).click(function() {
+
+               var target = $(this.hash);
+
+               $(‘html,body’).animate({
+
+                   scrollTop: target.offset().top
+
+               }, 1000);
+
+               return false;
+
+           });
+
+ 
+
+       });
+
+   </script>
