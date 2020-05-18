0307562f350cf5d830ddf324909a7bc3170e93bb
yunnn11 <61927834+yunnn11@users.noreply.github.com>
2020-05-18 20:17:46 +0800

    Add files via upload

---
+++ self/assets/js/timeline.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+	var progress_circle = $(".my-progress-bar"). circularProgress({
+
+    	line_width:12,
+    	color:"#CB997E",
+    	starting_position:0,
+    	percent:0,
+		percentage:true,
+}).circularProgress('animate', 65, 3000);
+})
+$(document).ready(function(){
+	var progress_circle = $(".my-progress-bar1"). circularProgress({
+
+    	line_width:12,
+    	color:"#CB997E",
+    	starting_position:0,
+    	percent:0,
+		percentage:true,
+}).circularProgress('animate', 90, 3000);
+})
+$(document).ready(function(){
+	var progress_circle = $(".my-progress-bar2"). circularProgress({
+
+    	line_width:12,
+    	color:"#CB997E",
+    	starting_position:0,
+    	percent:0,
+		percentage:true,
+}).circularProgress('animate', 80, 3000);
+})
+$(document).ready(function(){
+    var progress_circle = $(".my-progress-bar3"). circularProgress({
+
+        line_width:12,
+        color:"#CB997E",
+        starting_position:0,
+        percent:0,
+        percentage:true,
+}).circularProgress('animate', 70, 3000);
+})
+$(document).ready(function(){
+	var progress_circle = $(".my-progress-bar4"). circularProgress({
+
+    	line_width:12,
+    	color:"#CB997E",
+    	starting_position:0,
+    	percent:0,
+		percentage:true,
+}).circularProgress('animate', 95, 3000);
+})
+$(document).ready(function(){
+    var progress_circle = $(".my-progress-bar5"). circularProgress({
+
+        line_width:12,
+        color:"#CB997E",
+        starting_position:0,
+        percent:0,
+        percentage:true,
+}).circularProgress('animate', 80, 3000);
+})
